186b43970f356c4a3f66d756130a9aefaa553990
1bengardner <1bengardner@gmail.com>
2013-12-19 21:04:36 -0500

    added light

---
+++ General3DPlane/TrafficLight.cpp
@@ -0,0 +1,77 @@
+#include <windows.h>
+#include <gl/gl.h>
+#include <gl/freeglut.h>
+
+void drawTrafficLight(double size) {
+	glColor3f(0.1, 0.1, 0.1);
+	glBegin(GL_QUADS);
+		glVertex3f(-size/2, -size*1.5, -size/2);
+		glVertex3f( size/2, -size*1.5, -size/2);
+		glVertex3f( size/2, -size*1.5,  size/2);
+		glVertex3f(-size/2, -size*1.5,  size/2);
+
+		glVertex3f(-size/2, -size*1.5, -size/2);
+		glVertex3f( size/2, -size*1.5, -size/2);
+		glVertex3f( size/2,  size*1.5, -size/2);
+		glVertex3f(-size/2,  size*1.5, -size/2);
+
+		glVertex3f(-size/2, -size*1.5, -size/2);
+		glVertex3f(-size/2, -size*1.5,  size/2);
+		glVertex3f(-size/2,  size*1.5,  size/2);
+		glVertex3f(-size/2,  size*1.5, -size/2);
+
+		glVertex3f(-size/2, -size*1.5,  size/2);
+		glVertex3f( size/2, -size*1.5,  size/2);
+		glVertex3f( size/2,  size*1.5,  size/2);
+		glVertex3f(-size/2,  size*1.5,  size/2);
+
+		glVertex3f( size/2, -size*1.5, -size/2);
+		glVertex3f( size/2, -size*1.5,  size/2);
+		glVertex3f( size/2,  size*1.5,  size/2);
+		glVertex3f( size/2,  size*1.5, -size/2);
+		
+		glVertex3f(-size/2,  size*1.5, -size/2);
+		glVertex3f( size/2,  size*1.5, -size/2);
+		glVertex3f( size/2,  size*1.5,  size/2);
+		glVertex3f(-size/2,  size*1.5,  size/2);
+	glEnd();
+
+	glPushMatrix();
+		glColor3f(1, 0, 0);
+		glTranslatef(0, 0, size/2);
+		glutSolidSphere(size/2, 8, 8);
+	glPopMatrix();
+
+}
+
+void display() {
+	//clear the screen
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	drawTrafficLight(2);
+	glutSwapBuffers();
+}
+
+void main(int argc, char** argv) {
+	glutInit(&argc, argv);
+	glutInitWindowSize(800, 800);
+	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
+	glutCreateWindow("Traffic Light");
+
+	//enable Z buffer test, otherwise things appear in the order they're drawn
+	glEnable(GL_DEPTH_TEST);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluPerspective(20, 1, 1, 50);
+	gluLookAt(5, 0, 30, 0, 0, 0, 0, 1, 0);
+
+	//set clear colour to black
+	glClearColor(0, 0, 0, 0);
+
+	glMatrixMode(GL_MODELVIEW);
+
+	glutDisplayFunc(display);
+
+	//start the program!
+	glutMainLoop();
+}
