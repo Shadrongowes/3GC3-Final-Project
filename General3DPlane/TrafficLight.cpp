4450315f513260efd22ce88f8b490752f6976b7e
1bengardner <1bengardner@gmail.com>
2013-12-19 22:46:51 -0500

    new light

--- General3DPlane/TrafficLight.cpp
+++ General3DPlane/TrafficLight.cpp
@@ -2,54 +2,54 @@
 #include <gl/gl.h>
 #include <gl/freeglut.h>
 
-void drawTrafficLight(double size) {
-	glColor3f(0.1, 0.1, 0.1);
+void drawTrafficLight(float size, GLfloat topEmissive[], GLfloat middleEmissive[], GLfloat bottomEmissive[]) {
+	glColor3f(0.1f, 0.1f, 0.1f);
 	glBegin(GL_QUADS);
-		glVertex3f(-size/2, -size*1.5, -size/2);
-		glVertex3f( size/2, -size*1.5, -size/2);
-		glVertex3f( size/2, -size*1.5,  size/2);
-		glVertex3f(-size/2, -size*1.5,  size/2);
+		glVertex3f(-size/2, -size*1.5f, -size/2);
+		glVertex3f( size/2, -size*1.5f, -size/2);
+		glVertex3f( size/2, -size*1.5f,  size/2);
+		glVertex3f(-size/2, -size*1.5f,  size/2);
 
-		glVertex3f(-size/2, -size*1.5, -size/2);
-		glVertex3f( size/2, -size*1.5, -size/2);
-		glVertex3f( size/2,  size*1.5, -size/2);
-		glVertex3f(-size/2,  size*1.5, -size/2);
+		glVertex3f(-size/2, -size*1.5f, -size/2);
+		glVertex3f( size/2, -size*1.5f, -size/2);
+		glVertex3f( size/2,  size*1.5f, -size/2);
+		glVertex3f(-size/2,  size*1.5f, -size/2);
 
-		glVertex3f(-size/2, -size*1.5, -size/2);
-		glVertex3f(-size/2, -size*1.5,  size/2);
-		glVertex3f(-size/2,  size*1.5,  size/2);
-		glVertex3f(-size/2,  size*1.5, -size/2);
+		glVertex3f(-size/2, -size*1.5f, -size/2);
+		glVertex3f(-size/2, -size*1.5f,  size/2);
+		glVertex3f(-size/2,  size*1.5f,  size/2);
+		glVertex3f(-size/2,  size*1.5f, -size/2);
 
-		glVertex3f(-size/2, -size*1.5,  size/2);
-		glVertex3f( size/2, -size*1.5,  size/2);
-		glVertex3f( size/2,  size*1.5,  size/2);
-		glVertex3f(-size/2,  size*1.5,  size/2);
+		glVertex3f(-size/2, -size*1.5f,  size/2);
+		glVertex3f( size/2, -size*1.5f,  size/2);
+		glVertex3f( size/2,  size*1.5f,  size/2);
+		glVertex3f(-size/2,  size*1.5f,  size/2);
 
-		glVertex3f( size/2, -size*1.5, -size/2);
-		glVertex3f( size/2, -size*1.5,  size/2);
-		glVertex3f( size/2,  size*1.5,  size/2);
-		glVertex3f( size/2,  size*1.5, -size/2);
+		glVertex3f( size/2, -size*1.5f, -size/2);
+		glVertex3f( size/2, -size*1.5f,  size/2);
+		glVertex3f( size/2,  size*1.5f,  size/2);
+		glVertex3f( size/2,  size*1.5f, -size/2);
 		
-		glVertex3f(-size/2,  size*1.5, -size/2);
-		glVertex3f( size/2,  size*1.5, -size/2);
-		glVertex3f( size/2,  size*1.5,  size/2);
-		glVertex3f(-size/2,  size*1.5,  size/2);
+		glVertex3f(-size/2,  size*1.5f, -size/2);
+		glVertex3f( size/2,  size*1.5f, -size/2);
+		glVertex3f( size/2,  size*1.5f,  size/2);
+		glVertex3f(-size/2,  size*1.5f,  size/2);
 	glEnd();
 
 	glPushMatrix();
-		glColor3f(1, 0, 0);
+		glMaterialfv(GL_FRONT, GL_EMISSION, topEmissive);
 		glTranslatef(0, size, size/2);
 		glutSolidSphere(size/3, 16, 16);
 	glPopMatrix();
 
 	glPushMatrix();
-		glColor3f(1, 0.5, 0);
+		glMaterialfv(GL_FRONT, GL_EMISSION, middleEmissive);
 		glTranslatef(0, 0, size/2);
 		glutSolidSphere(size/3, 16, 16);
 	glPopMatrix();
 
 	glPushMatrix();
-		glColor3f(0, 1, 0);
+		glMaterialfv(GL_FRONT, GL_EMISSION, bottomEmissive);
 		glTranslatef(0, -size, size/2);
 		glutSolidSphere(size/3, 16, 16);
 	glPopMatrix();
@@ -57,10 +57,13 @@ void drawTrafficLight(double size) {
 }
 
 void display() {
+	GLfloat material_emissive_red[] = {1.0f, 0.0f, 0.0f, 0.0f};
+	GLfloat material_emissive_orange[] = {1.0f, 0.4f, 0.0f, 0.0f};
+	GLfloat material_emissive_green[] = {0.0f, 1.0f, 0.0f, 0.0f};
+
 	//clear the screen
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-	drawTrafficLight(1);
+	drawTrafficLight(1, material_emissive_red, material_emissive_orange, material_emissive_green);
 	glutSwapBuffers();
 }
 
@@ -69,6 +72,21 @@ void main(int argc, char** argv) {
 	glutInitWindowSize(600, 600);
 	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
 	glutCreateWindow("Traffic Light");
+
+	//Light
+	glEnable(GL_LIGHTING);
+	GLfloat light_ambient[] = {1.0f, 1.0f, 1.0f, 1.0f};
+	GLfloat light_diffuse[] = {1.0f, 1.0f, 1.0f, 1.0f};
+	GLfloat light_specular[] = {1.0f, 1.0f, 1.0f, 1.0f};
+	GLfloat light_position[] = {0.0f, 1.0f, 5.0f, 0.0f};
+
+	//assign light qualities to a light
+	glLightfv(GL_LIGHT0, GL_AMBIENT, light_ambient);
+	glLightfv(GL_LIGHT0, GL_DIFFUSE, light_diffuse);
+	glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular);
+	glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+
+	glEnable(GL_LIGHT0);
 
 	//enable Z buffer test, otherwise things appear in the order they're drawn
 	glEnable(GL_DEPTH_TEST);
